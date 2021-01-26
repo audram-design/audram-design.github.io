4d9a4efab7d9a3f9d34f97315bf0e8260316645d
audram-design <69908449+audram-design@users.noreply.github.com>
2021-01-26 11:53:43 -0700

    jquery continued iteration

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function toggleMenu(){
-    var x = document.getElementById("navbar");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+    var links = document.getElementById("links");
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
   }
-
-
-}
+  
+  function resized() {
+  
+    var links = document.getElementById("links");
+  
+    if (window.innerWith >= 600) {
+      links.style.display = "block";
+    } else {
+      links.style.display = "none";
+    }
+  }
